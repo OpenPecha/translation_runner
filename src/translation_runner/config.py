11d3f0c3bb9eeb689302a192f2015aa1ebd39f0a
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-05-06 05:51:45 +0000

    Add output path config and helper function to create directory

--- src/translation_runner/config.py
+++ src/translation_runner/config.py
@@ -1,1 +1,12 @@
+from pathlib import Path
+
+
+def _mkdir(path):
+    if path.is_dir():
+        return path
+    path.mkdir(exist_ok=True, parents=True)
+    return path
+
+
 API_BASE_URL = "https://api-aq25662yyq-uc.a.run.app/pecha/"
+OUTPUT_PATH = _mkdir(Path.home() / "translation_runner")
