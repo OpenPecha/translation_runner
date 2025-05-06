16102c8617b9cbea9bf48fe54e532c18154cf4ef
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-05-06 10:11:45 +0000

    fixDEVELOPMENT_URL

--- src/translation_runner/config.py
+++ src/translation_runner/config.py
@@ -12,7 +12,7 @@ OUTPUT_PATH = _mkdir(Path.home() / "translation_runner")
 
 
 PRODUCTION_URL = "https://api-aq25662yyq-uc.a.run.app"
-DEVELOPMENT_URL = "https://api-125bgmwqoa-uc.a.run.app"
+DEVELOPMENT_URL = "https://api-l25bgmwqoa-uc.a.run.app"
 
 DOWNLOAD_PECHA_PROD = f"{PRODUCTION_URL}/pecha/"
 DOWNLOAD_PECHA_DEV = f"{DEVELOPMENT_URL}/pecha/"
