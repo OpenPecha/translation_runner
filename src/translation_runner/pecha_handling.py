4471b4ecb0d39ecbdab31ebc47ec8d11ffabc0cd
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-05-06 05:53:41 +0000

    get root and commentary pecha

--- src/translation_runner/pecha_handling.py
+++ src/translation_runner/pecha_handling.py
@@ -2,9 +2,17 @@ from pathlib import Path
 
 from openpecha.pecha import Pecha
 
+from translation_runner.config import OUTPUT_PATH
 from translation_runner.utils import download_pecha
 
 
 def get_pecha(pecha_id: str, output_path: Path) -> Pecha:
     pecha_path = download_pecha(pecha_id, output_path)
     return Pecha.from_path(pecha_path)
+
+
+def get_alignment(root_id: str, commentary_id: str, output_path: Path = OUTPUT_PATH):
+    root_pecha = get_pecha(root_id, output_path)
+    commentary_pecha = get_pecha(commentary_id, output_path)
+
+    pass
